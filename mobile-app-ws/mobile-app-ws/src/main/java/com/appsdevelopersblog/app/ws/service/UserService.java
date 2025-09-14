d16322bd45d1f9ba2ee3d71d832b908051d51d25
Necmettin Gedikli <104091365+NecmettinG@users.noreply.github.com>
2025-09-14 20:07:21 +0300

    User login request model and loadUserByUsername() implemented

--- mobile-app-ws/mobile-app-ws/src/main/java/com/appsdevelopersblog/app/ws/service/UserService.java
+++ mobile-app-ws/mobile-app-ws/src/main/java/com/appsdevelopersblog/app/ws/service/UserService.java
@@ -3,7 +3,7 @@ package com.appsdevelopersblog.app.ws.service;
 import com.appsdevelopersblog.app.ws.shared.dto.UserDto;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
-public interface UserService extends UserDetailsService {
+public interface UserService extends UserDetailsService {//UserDetailsService comes from spring and it is used for authentication.
 
     public UserDto createUser(UserDto user);
 }
--- mobile-app-ws/mobile-app-ws/src/main/java/com/appsdevelopersblog/app/ws/service/impl/UserServiceImpl.java
+++ mobile-app-ws/mobile-app-ws/src/main/java/com/appsdevelopersblog/app/ws/service/impl/UserServiceImpl.java
@@ -7,10 +7,13 @@ import com.appsdevelopersblog.app.ws.shared.Utils;
 import com.appsdevelopersblog.app.ws.shared.dto.UserDto;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -53,6 +56,21 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+
+        UserEntity userEntity = userRepository.findByEmail(username);
+
+        if(userEntity == null){
+
+            throw new UsernameNotFoundException("email not found.");
+        }
+
+        return new User(username, userEntity.getEncryptedPassword(), new ArrayList<>());
+        //We are supposed to return a User instance in loadUserByUsername function. User implements UserDetails from spring.
+        //First parameter will be username(email), second is encrypted password that will be decoded automatically.
+        //and the third is a list of granted authorities, which are users roles and permissions.
+        //We are going to check if these parameters are matching with the user data from login screen.
+
+        //When we send a http request for login with username and password data,
+        // Spring will invoke this method to locate user details from database.
     }
 }
