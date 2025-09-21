11d38d54161eba29b9804fe110d41df8947be369
Necmettin Gedikli <104091365+NecmettinG@users.noreply.github.com>
2025-09-21 19:08:45 +0300

    Added Authentication Filter, Http Security, and JWT
    Very explanatory comments for security part are also added.

---
+++ mobile-app-ws/mobile-app-ws/src/main/java/com/appsdevelopersblog/app/ws/security/AuthenticationFilter.java
@@ -0,0 +1,95 @@
+package com.appsdevelopersblog.app.ws.security;
+
+import com.appsdevelopersblog.app.ws.ui.model.request.UserLoginRequestModel;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.IOException;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.Date;
+
+/*This class will be used to authenticate user when they send request to perform user login.
+* When application receives a HTTP request to perform user login, this filter class will be triggered.
+* It will read username and password from HTTP request and pass them to spring framework.
+* Spring will validate these provided user credentials and if they are correct, we will be able to generate access token.*/
+public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+//UsernamePasswordAuthenticationFilter is a spring security class for processing authentication information when it is submitted in HTTP request.
+
+    //AuthenticationManager is used during authentication process and it has only one method called "authenticate".
+    public AuthenticationFilter(AuthenticationManager authenticationManager) {
+        super(authenticationManager);
+    }
+
+    //attemptAuthentication method is a part of UsernamePasswordAuthenticationFilter class, and we are overriding it.
+    //It receives two parameters which are Http request and Http response objects.
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest req, HttpServletResponse res)
+            throws AuthenticationException {
+        try {
+
+            //We are going to read our json payload from Http request and these information will be mapped into UserLoginRequestModel object.
+            //UserLoginRequestModel class is created by us, and it has attributes of email and password.
+            UserLoginRequestModel creds = new ObjectMapper().readValue(req.getInputStream(), UserLoginRequestModel.class);
+
+            //We created UsernamePasswordAuthenticationToken object with using UserLoginRequestModel instance.
+            //UsernamePasswordAuthenticationToken is a simple class for representing username and password.
+            //We pass this simple class's object as a parameter to authenticate method to trigger authentication process.
+            //After invoking authenticate method, spring will invoke loadUserByUsername method from UserServiceImpl class.
+
+//If provided username and password are verified and correct,
+// the authentication will be successful and successfulAuthentication method will be invoked by spring.
+            return getAuthenticationManager().authenticate(
+                    new UsernamePasswordAuthenticationToken(creds.getEmail(), creds.getPassword(), new ArrayList<>()));
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    //Once authentication is successful, this method will be invoked by spring.
+    //In this method, we are going to generate jwt access token and return this token in http response header.
+    @Override
+    protected void successfulAuthentication(HttpServletRequest req, HttpServletResponse res, FilterChain chain,
+                                            Authentication auth) throws IOException, ServletException {
+
+        //To generate JSON web token, We used the value of Token Secret from SecurityConstants class and got its bytes.
+        //Then we used Base64 encoder to encode these bytes into a new byte array called secretKeyBytes.
+        byte[] secretKeyBytes = Base64.getEncoder().encode(SecurityConstants.TOKEN_SECRET.getBytes());
+
+        //We used the byte array to generate secret key which will be used to sign jwt access token in the code below.
+        SecretKey secretKey = new SecretKeySpec(secretKeyBytes, SignatureAlgorithm.HS512.getJcaName());
+
+        //For setting generation date and expiration date for jwt token, We use Instant object.
+        Instant now = Instant.now();
+
+        //We used auth object for getting authenticated username.
+        String userName = ((User) auth.getPrincipal()).getUsername();
+
+        //To generate jwt token, we use Jwts.builder().
+        String token = Jwts.builder()
+                .setSubject(userName) //We used authenticated username as a subject to jwt token.
+                .setExpiration( //Setting token expiration time with using Expiration Time from SecurityConstants class.
+                        Date.from(now.plusMillis(SecurityConstants.EXPIRATION_TIME)))
+                .setIssuedAt(Date.from(now)).signWith(secretKey, SignatureAlgorithm.HS512).compact();
+        //setting generation time and signing jwt access token with a secret key. compact method will return a final value of jwt access token.
+
+        //access token is generated and added as a header to a http response object.
+        //When the client application receives http response, it will be able to read this jwt access token from http response header.
+        res.addHeader(SecurityConstants.HEADER_STRING, SecurityConstants.TOKEN_PREFIX + token);
+    }
+}
--- mobile-app-ws/mobile-app-ws/src/main/java/com/appsdevelopersblog/app/ws/security/WebSecurity.java
+++ mobile-app-ws/mobile-app-ws/src/main/java/com/appsdevelopersblog/app/ws/security/WebSecurity.java
@@ -5,36 +5,60 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+//This class is for http web security.
 @Configuration
 @EnableWebSecurity
 public class WebSecurity {
 
-    private final UserService userDetailsService;
+    private final UserService userDetailsService; //Our UserService interface extends UserDetailsService interface.
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public WebSecurity(UserService userDetailsService, BCryptPasswordEncoder bCryptPasswordEncoder){
+    public WebSecurity(UserService userDetailsService, BCryptPasswordEncoder bCryptPasswordEncoder) {
 
         this.userDetailsService = userDetailsService;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
+    /*This method will accept HttpSecurity object as a parameter.
+    Spring will call this method automatically due to @Bean annotation. At the application start up by the way.
+    Our code will be executed and HttpSecurity object will be added to Application Context. Spring will use it whenever it needs to use.
+    When we send the HTTP request to an API endpoint, spring will take this request through a chain of filters,
+    and one of the filters will validate the HTTP request against the security configuration that we will configure in this method.*/
     @Bean
     SecurityFilterChain configure(HttpSecurity http) throws Exception {
 
+        //We will use Http security object to get a shared object of AuthenticationManagerBuilder class.
+        //We will use this AuthenticationManagerBuilder object to configure which service class in our application will be responsible to load-
+        // -user details from database and when user performs logging, spring will use this class to see if our database does have a user with-
+        // -provided login credentials.
         AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
 
+        //We will use AuthenticationManagerBuilder's instance to specify which class in our app will be used to look up user details in the-
+        // -database. We are required to pass our UserDetailsService object to userDetailsService() method. For spring framework to be able to-
+        // -encode the provided user password, and then check if the password that we stored in the database does match it, We need to tell-
+        // -spring that which password encoder we used. We used bcrypt password encoder in our app to encode user passwords.
         authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
 
-        http.csrf((csrf)-> csrf.disable())
-                .authorizeHttpRequests((authz) ->authz.requestMatchers(HttpMethod.POST, "/users")
-                        .permitAll().anyRequest().authenticated());
+        //We built our authentication manager and we will use this object and our AuthenticationFilter class to create new authentication filter.
+        AuthenticationManager authenticationManager = authenticationManagerBuilder.build();
 
-        return http.build();
+        http.csrf((csrf) -> csrf.disable()) //We disabled cross-site request forgery which is redundant for our app. Because our api is stateless.
+                .authorizeHttpRequests((authz) -> authz.requestMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL)
+                        .permitAll().anyRequest().authenticated())//We made post request on /users api endpoint public and we won't get http 403.
+                .authenticationManager(authenticationManager).addFilter(new AuthenticationFilter(authenticationManager));
+                //We add new authentication filter with creating AuthenticationFilter object, and AuthenticationManager object will be passed-
+                // -to super class constructor. We also updated Http security object with .authenticationManager(authenticationManager) method.
+
+        // /users api endpoint will be public and open to all users, and all other api endpoints will be protected.
+        // Only authenticated users are allowed to invoke them.
+
+        return http.build(); //We are going to build and return http security object from this method.
     }
 }
