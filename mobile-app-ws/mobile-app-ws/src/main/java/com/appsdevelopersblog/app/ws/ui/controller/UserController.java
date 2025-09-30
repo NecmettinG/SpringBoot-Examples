9cf4304fd7a1855c8b23c77d556fccfa2eeca72e
Necmettin Gedikli <104091365+NecmettinG@users.noreply.github.com>
2025-09-30 14:27:02 +0300

    Added XML and JSON support to our API.

--- mobile-app-ws/mobile-app-ws/src/main/java/com/appsdevelopersblog/app/ws/ui/controller/UserController.java
+++ mobile-app-ws/mobile-app-ws/src/main/java/com/appsdevelopersblog/app/ws/ui/controller/UserController.java
@@ -8,6 +8,7 @@ import com.appsdevelopersblog.app.ws.ui.model.request.UserDetailsRequestModel;
 import com.appsdevelopersblog.app.ws.ui.model.response.UserRest;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,7 +18,12 @@ public class UserController{
     @Autowired
     UserServiceImpl userService;
 
-    @GetMapping(path = "/{id}")
+    //We are going to add xml support for response. Default response format is Json but some client applications may want xml response.
+    //This response type is defined in Http header called "Accept". It may take values like "applicaton/json", "application/xml" and so on.
+    //Postman has wildcards for response types. If we type "*/*" in Accept header, Postman will accept all types of responses.
+    //With "produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}", We added both Json and Xml response support.
+    //But Json is prioritized because it is written as first. If client app. doesn't define "Accept" header, Default response will be Json.
+    @GetMapping(path = "/{id}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public UserRest getUser(@PathVariable("id") String id){
 
         UserRest returnValue = new UserRest();
@@ -29,7 +35,11 @@ public class UserController{
         return returnValue;
     }
 
-    @PostMapping
+    //Our Post request method can accept User Details as Xml format currently. We implemented Xml support in both request and response.
+    //"consumes" is responsible for request body. We define the format of the request input with the Http header called "Content-Type".
+    @PostMapping(
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public UserRest createUser(@RequestBody UserDetailsRequestModel userDetails){
 
         UserRest returnValue = new UserRest();//This instance is for response.
