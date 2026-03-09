68236c891cbf154177b3c87a38a1a3c569850051
Necmettin Gedikli <104091365+NecmettinG@users.noreply.github.com>
2026-03-09 15:18:23 +0300

    The very first test class is created
    We will use JUnit for unit testing.

---
+++ mobile-app-ws/mobile-app-ws/src/test/java/com/appsdevelopersblog/app/ws/service/impl/UserServiceImplTest.java
@@ -0,0 +1,20 @@
+package com.appsdevelopersblog.app.ws.service.impl;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserServiceImplTest {
+
+    @BeforeEach
+    void setUp() throws Exception{
+
+
+    }
+
+    @Test
+    final void testGetUser(){
+
+        fail("Not Yet Implemented");
+    }
+}
