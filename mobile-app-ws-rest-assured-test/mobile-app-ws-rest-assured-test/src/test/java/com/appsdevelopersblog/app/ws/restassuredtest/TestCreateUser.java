736a1a66994e89d371b30181c63d6d43a19528b5
Necmettin Gedikli <104091365+NecmettinG@users.noreply.github.com>
2026-03-18 17:11:46 +0300

    Add RestAssured test for creating user
    Introduce TestCreateUser.java: a JUnit 5 integration test that configures RestAssured (http://localhost:8080), builds a JSON request body using Map/List for user details and addresses, posts to /mobile-app-ws/users, asserts HTTP 200 and JSON content type, and verifies the response contains a non-null userId.

---
+++ mobile-app-ws-rest-assured-test/mobile-app-ws-rest-assured-test/src/test/java/com/appsdevelopersblog/app/ws/restassuredtest/TestCreateUser.java
@@ -0,0 +1,75 @@
+package com.appsdevelopersblog.app.ws.restassuredtest;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class TestCreateUser {
+
+
+    private final String CONTEXT_PATH = "/mobile-app-ws";
+
+    @BeforeEach
+    void setUp() throws Exception {
+
+        //We declared our base URI and port number here.
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = 8080;
+    }
+
+    @Test
+    final void testCreateUser() {
+
+        //These are for creating a request body with using hashmaps!
+        //You can also examine the structure of the hashmaps! These are important in technical assessments!
+
+        //creating this arraylist with map for storing user addresses.
+        List<Map<String, Object>> userAddresses = new ArrayList<>();
+
+        //This is a single user address created with hashmap.
+        Map<String, Object> shippingAddress = new HashMap<>();
+        shippingAddress.put("city", "Istanbul");
+        shippingAddress.put("country", "Turkey");
+        shippingAddress.put("streetName", "aydinlar street");
+        shippingAddress.put("postalCode", "15963");
+        shippingAddress.put("type", "shipping");
+
+        //We add user address hashmap into our arraylist named userAddresses.
+        userAddresses.add(shippingAddress);
+
+        //This hashmap is for creating request body of a user.
+        Map<String, Object> userDetails = new HashMap<>();
+        userDetails.put("firstName", "Necmettin");
+        userDetails.put("lastName", "Gedikli");
+        userDetails.put("email", "necmettingedikli611@gmail.com");
+        userDetails.put("password", "794378");
+        userDetails.put("addresses", userAddresses); //we put our userAddresses array list into our userDetails hashmap.
+
+        //We are creating a Response object. Json response will be parsed and put into response.
+        Response response = given(). //after given(), we will declare content type and application headers and request body!.
+                contentType("application/json").
+                accept("application/json").
+                body(userDetails).
+                when().//after when(), we will declare our http method with complete address.
+                post(CONTEXT_PATH + "/users").
+                then().//after then(), we will assert our status code as 200 and content type as json.
+                statusCode(200).
+                contentType("application/json").
+                extract(). // we will extract the entire json response body into Response object with using .extract().response()!
+                response();
+
+        //jsonPath returns json representation of the response body!
+        String userId = response.jsonPath().getString("userId");
+
+        assertNotNull(userId);
+    }
+}
