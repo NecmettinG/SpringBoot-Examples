3a022959d083233882188c058f4b480c6bc6b266
Necmettin Gedikli <104091365+NecmettinG@users.noreply.github.com>
2026-04-01 16:31:37 +0300

    Add RoleEntity and user-role many-to-many
    Introduce RoleEntity (roles table) and wire a many-to-many relationship between UserEntity and RoleEntity. RoleEntity is the inverse side (mappedBy="roles"); UserEntity now declares a roles Collection with @ManyToMany, @JoinTable(users_roles) using join columns users_id and roles_id, cascade = {PERSIST} and FetchType.EAGER for authentication needs. Also added getters/setters for roles and updated imports. Comments clarify ownership, cascade behavior, and fetch semantics. Additionally clarified OneToMany mapping comments for addresses (mappedBy userDetails, orphanRemoval=true).

---
+++ mobile-app-ws/mobile-app-ws/src/main/java/com/appsdevelopersblog/app/ws/io/entity/RoleEntity.java
@@ -0,0 +1,52 @@
+package com.appsdevelopersblog.app.ws.io.entity;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.Collection;
+
+@Entity
+@Table(name="roles")
+public class RoleEntity implements Serializable {
+
+    private static final long serialVersionUID = 153134934138596457L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(nullable = false, length = 20)
+    private String roleName;
+
+    //We declared many to many relationship between RoleEntity and UserEntity. One role can belong to multiple users.
+    //In a Many-To-Many relationship, one side must be the "owning" side that defines the join table, and the other side is the "inverse" side.
+    //Here, UserEntity is chosen as the owning side (it defines @JoinTable for "users_roles").
+    //"mappedBy" is placed on the inverse side (RoleEntity) and takes the name of the attribute from the owning side (UserEntity) that owns the relationship-
+    //-which in this case is "roles". This tells Hibernate not to create an additional join table for this side of the relationship.
+    @ManyToMany(mappedBy = "roles")
+    private Collection<UserEntity> users;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public Collection<UserEntity> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Collection<UserEntity> users) {
+        this.users = users;
+    }
+}
--- mobile-app-ws/mobile-app-ws/src/main/java/com/appsdevelopersblog/app/ws/io/entity/UserEntity.java
+++ mobile-app-ws/mobile-app-ws/src/main/java/com/appsdevelopersblog/app/ws/io/entity/UserEntity.java
@@ -3,6 +3,7 @@ package com.appsdevelopersblog.app.ws.io.entity;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.List;
 
 @Entity
@@ -38,11 +39,35 @@ public class UserEntity implements Serializable {
     private Boolean emailVerificationStatus = false;
 
     //We declared one to many relationship between UserEntity and AddressEntity. One user can have multiple addresses.
-    //"mappedBy" takes the name of the attribute, which will be used for foreign key column, that has @ManyToOne from AddressEntity.
+    //In a One-To-Many relationship, one side must be the "owning" side that holds the foreign key, and the other side is the "inverse" side.
+    //Here, AddressEntity is chosen as the owning side (it defines @JoinColumn for "users_id").
+    //"mappedBy" is placed on the inverse side (UserEntity) and takes the name of the attribute from the owning side (AddressEntity) that owns the relationship-
+    //-which in this case is "userDetails". This tells Hibernate that AddressEntity manages the foreign key, so it doesn't need to create an additional join table.
     //CascadeType.ALL means whenever we delete a user, their address records from "addresses" table will also be deleted. But for avoiding-
     //-errors whenever we try to delete directly from SQL, we have to add orphanRemoval = true.
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "userDetails", orphanRemoval = true)
     private List<AddressEntity> addresses;
+
+    /*Annotation for many to many relationship. If user is deleted, role won't be affected due to cascade = {CascadeType.PERSIST}. If we used-
+    cascade = CascadeType.ALL, whenever we delete a user, its role would also be deleted.
+    fetch = FetchType.EAGER performs aggressive fetch. When user details are read from the database, user roles are fetched from the database-
+    right away. Setting fetch type as eager is needed for authentication. Spring Framework will require that user roles.
+    
+    If we used fetch = FetchType.LAZY, it would perform on-demand fetch. When user details are read from the database, user roles would not be-
+    fetched immediately. They would only be fetched when the roles collection is explicitly accessed for the first time. This improves-
+    performance when related entities are not always needed, but in our case, we need them eagerly for authentication.*/
+    @ManyToMany(cascade = {CascadeType.PERSIST}, fetch = FetchType.EAGER)
+    /*
+    We are creating a joint table with this annotation with @JoinTable. In joinColumns parameter, We will create a column that will have a-
+    reference to the id of the user entity. name parameter in the @JoinColumn takes the column name and referencedColumnName parameter takes the-
+    attribute name in userEntity. Since we reference primary key of UserEntity in this column, we will write the attribute called "id".
+    inverseJoinColumns will reference the column of other entity. roles_id column will reference the id of a role record(entity).
+    */
+    @JoinTable(name = "users_roles",
+            joinColumns = @JoinColumn(name = "users_id",referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "roles_id",referencedColumnName = "id"))
+    //One user can have many roles so they need to be in a Collection.
+    private Collection<RoleEntity> roles;
 
     public long getId() {
         return id;
@@ -114,5 +139,15 @@ public class UserEntity implements Serializable {
 
     public void setAddresses(List<AddressEntity> addresses) {
         this.addresses = addresses;
+    }
+
+    public Collection<RoleEntity> getRoles(){
+
+        return roles;
+    }
+
+    public void setRoles(Collection<RoleEntity> roles){
+
+        this.roles = roles;
     }
 }
