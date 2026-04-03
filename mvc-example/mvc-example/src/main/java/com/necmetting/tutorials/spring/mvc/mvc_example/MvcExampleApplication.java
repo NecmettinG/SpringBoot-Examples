58e18b14078b30a9fedf1cc6dc6161043d5d3af6
Necmettin Gedikli <104091365+NecmettinG@users.noreply.github.com>
2026-04-03 19:53:51 +0300

    Add mvc-example Spring Boot project with wrapper
    Scaffold a new mvc-example Spring Boot MVC application (WAR). Adds pom.xml (includes tomcat-embed-jasper and provided Tomcat), application entrypoint (MvcExampleApplication), ServletInitializer for WAR deployment, basic application.properties and a simple context-load test. Also adds Maven Wrapper (mvnw, mvnw.cmd, .mvn/wrapper/maven-wrapper.properties), .gitignore and .gitattributes. Additionally adds an Architecture of Spring MVC PNG to the mobile-app-ws module.

---
+++ mvc-example/mvc-example/src/main/java/com/necmetting/tutorials/spring/mvc/mvc_example/MvcExampleApplication.java
@@ -0,0 +1,13 @@
+package com.necmetting.tutorials.spring.mvc.mvc_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcExampleApplication.class, args);
+	}
+
+}
---
+++ mvc-example/mvc-example/src/test/java/com/necmetting/tutorials/spring/mvc/mvc_example/MvcExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.necmetting.tutorials.spring.mvc.mvc_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
