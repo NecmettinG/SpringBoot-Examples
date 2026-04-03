848f52255b065c41cd705abf4a86816574abfd6a
Necmettin Gedikli <104091365+NecmettinG@users.noreply.github.com>
2026-04-03 21:21:31 +0300

    Migrate home view to Thymeleaf
    Replace legacy JSP view with a Thymeleaf template and update configuration/docs. Added src/main/resources/templates/home.html (Thymeleaf) and removed WEB-INF/jsp/home.jsp. Commented out JSP view resolver properties in application.properties so Spring Boot's Thymeleaf auto-configuration can be used. Also clarified HomeController javadoc to explain view resolution for Thymeleaf vs JSP.

--- mvc-example/mvc-example/src/main/java/com/necmetting/tutorials/spring/mvc/mvc_example/HomeController.java
+++ mvc-example/mvc-example/src/main/java/com/necmetting/tutorials/spring/mvc/mvc_example/HomeController.java
@@ -9,7 +9,10 @@ public class HomeController {
     /**
      * Handles GET requests to the root URL ("/").
      * The @GetMapping annotation maps the HTTP GET request to this method.
-     * Returning "home" tells Spring MVC to look for a view template named "home" (e.g., home.jsp).
+     * 
+     * Returning "home" tells Spring MVC to look for a view template named "home".
+     * - If you are using Thymeleaf (the default modern approach), Spring looks for 'home.html' inside the 'src/main/resources/templates/' directory.
+     * - If you are using JSP (the legacy approach), Spring typically looks for 'home.jsp' configured inside the '/WEB-INF/jsp/' directory.
      */
     @GetMapping("/")
     public String homePage(){
